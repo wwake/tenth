90f523dcdfd9f39bcd8ca4bcb78ac42e3dcff664
Bill Wake <william.wake@acm.org>
2024-03-04 14:50:56 -0500

    Reorganize assertion messages

--- asUnit.s
+++ asUnit.s
@@ -1,6 +1,15 @@
 .global assertEqual
 
+.text
 .align 2
+L_passMessage:
+.asciz "\033[32mPass\033[0m\n"
+
+L_failedMessage:
+.asciz "\033[31mFail\033[0m\n"
+
+.align 2
+
 // assertEqual: prints pass if x0 == x1, or fail message if x0 != x1
 assertEqual:
 	str lr, [sp, #-16]!
@@ -8,22 +17,16 @@ assertEqual:
 	cmp x0, x1
 	bne L_failed
 	
-	adr x0, passMessage
+	adr x0, L_passMessage
 	b L_exiting
 	
 L_failed:
-	adr x0, failedMessage
+	adr x0, L_failedMessage
 
 L_exiting:
 	bl print
 
 	ldr lr, [sp], #16
 	ret
-
-passMessage:
-	.asciz "\033[32mPass\033[0m\n"
-
-failedMessage:
-	.ascii "\033[31mFail\033[0m\n"
 
 .align 2
