6091f381f4aa775b09236aa9f68087b8dfde14f0
Bill Wake <william.wake@acm.org>
2024-03-04 15:02:11 -0500

    Make assertEqual tell x0/x1 when it fails

--- asUnit.s
+++ asUnit.s
@@ -6,26 +6,54 @@ L_passMessage:
 .asciz "\033[32mPass\033[0m\n"
 
 L_failedMessage:
-.asciz "\033[31mFail\033[0m\n"
+.asciz "\033[31mFail: x0="
+
+L_x1:
+.asciz " x1="
+
+L_failMessageEnd:
+.asciz "\033[0m\n"
 
 .align 2
 
 // assertEqual: prints pass if x0 == x1, or fail message if x0 != x1
 assertEqual:
 	str lr, [sp, #-16]!
-			
+	str x10, [sp, #8]
+	str x11, [sp, #-16]!
+
+	mov x10, x0
+	mov x11, x1
+
 	cmp x0, x1
 	bne L_failed
 	
 	adr x0, L_passMessage
+	bl print
+
 	b L_exiting
 	
 L_failed:
 	adr x0, L_failedMessage
-
-L_exiting:
 	bl print
 
+	mov x0, x10
+	bl dec2str
+	bl print
+
+	adr x0, L_x1
+	bl print
+
+	mov x0, x11
+	bl dec2str
+	bl print
+
+	adr x0, L_failMessageEnd
+	bl print
+
+L_exiting:
+	ldr x11, [sp], #16
+	ldr x10, [sp, #8]
 	ldr lr, [sp], #16
 	ret
 
