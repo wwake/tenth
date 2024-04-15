b86b54ad8545f9a3c39c413e5b77bec37f442bba
Bill Wake <william.wake@acm.org>
2024-04-15 14:34:49 -0400

    Add head$

--- string.s
+++ string.s
@@ -2,6 +2,7 @@
 #include "assembler.macros"
 
 .global make_string
+.global head_string
 
 .text
 .align 2
@@ -28,4 +29,19 @@ L_make_exiting:
 	strb wzr, [SEC_SPACE], #1
 	ROUND_UP_8 SEC_SPACE
 
+	ret
+
+
+// head_string - pop address of string, push address of string with first character removed, and push that character (as an integer)
+//
+head_string:
+	DATA_POP x0
+	ldrb w1, [x0], #1
+	
+	cmp w1, #0
+	b.eq L_end_head_string
+	DATA_PUSH x0
+
+L_end_head_string:
+	DATA_PUSH x1
 	ret
